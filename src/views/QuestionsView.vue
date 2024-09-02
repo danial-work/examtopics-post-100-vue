a77c923497732409496368b1eb5bca177a50b467
unknown <Kasatria@PC-86.ad.kasatria.com>
2024-09-02 18:33:29 +0800

    first add

---
+++ src/views/QuestionsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/question?qs=1&qe=10">Questions</router-link>
+  </nav>
+  <div>
+    <h1>Questions</h1>
+    
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log(route.query)
+</script>
